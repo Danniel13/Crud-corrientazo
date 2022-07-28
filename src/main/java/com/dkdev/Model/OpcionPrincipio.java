083dfd2977f0ed9a75999952b54f25bc06b8737a
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-07-27 21:13:23 -0500

    Commit_structure

---
+++ src/main/java/com/dkdev/Model/OpcionPrincipio.java
@@ -0,0 +1,14 @@
+package com.dkdev.Model;
+
+public class OpcionPrincipio {
+  private String nombre;
+
+  public OpcionPrincipio(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+  
+}
---
+++ src/main/java/com/dkdev/Model/Opcionpedido.java
@@ -0,0 +1,28 @@
+package com.dkdev.Model;
+
+public abstract class Opcionpedido {
+  
+
+  private Integer Precio;
+
+
+
+
+  //CONSTRUCTOR
+  public Opcionpedido(Integer precio) {
+    Precio = precio;
+  }
+
+
+
+
+  public Integer getPrecio() {
+    return Precio;
+  }
+  
+//GETTER:
+
+ 
+
+
+}
