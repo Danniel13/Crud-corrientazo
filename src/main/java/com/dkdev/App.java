5966122ae29460454aff2faff9c271babb344395
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-08-01 14:08:01 -0500

    Commit_MVC

--- src/main/java/com/dkdev/App.java
+++ src/main/java/com/dkdev/App.java
@@ -3,15 +3,63 @@ package com.dkdev;
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
-    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+
+import com.dkdev.Model.Adicional;
+import com.dkdev.Model.Bandeja;
+import com.dkdev.Model.Completo;
+import com.dkdev.Model.Mesa;
+import com.dkdev.Model.OpcionCarne;
+import com.dkdev.Model.OpcionEnsalada;
+import com.dkdev.Model.OpcionJugo;
+import com.dkdev.Model.OpcionPrincipio;
+import com.dkdev.Model.Pedido;
+import com.dkdev.Model.Opcionsopa;
+import com.dkdev.View.PrincipalVista;
+import com.dkdev.exceptions.EfectivoInsuficienteException;
+
+
+
+
+public final class App {
+  public static void main(String[] args) {
+      //prueba();
+      var menu = new PrincipalVista();
+      menu.iniciarAplicacion();
+  }
+
+  private static void prueba() {
+      var mesa = new Mesa("01");
+
+      var sopa = new Opcionsopa("Pasta");
+      var principio = new OpcionPrincipio("Frijoles");
+      var carne = new OpcionCarne("Res a la plancha");
+      var ensalada = new OpcionEnsalada("Rusa");
+      var jugo = new OpcionJugo("Limonada");
+
+      var cesar = new Pedido("Cesar", new Completo(12_000, sopa, principio, carne, ensalada, jugo));
+      cesar.agregarAdicional(new Adicional("Huevo", 1_000));
+      cesar.agregarAdicional(new Adicional("Gaseosa", 2_000));
+
+      var laura = new Pedido("Laura", new Bandeja(10_000, principio, carne, jugo));
+
+      mesa.adicionarPedido(cesar);
+      mesa.adicionarPedido(laura);
+
+      // cesar.entregar();
+      // laura.entregar();
+
+      var total = mesa.calcularValorPagar();
+      System.out.printf("La mesa %s debe $ %,d %n", mesa.getNumero(), total);
+
+      try {
+          var efectivo = 24_000;
+          var devuelta = mesa.pagar(efectivo);
+          System.out.printf("La mesa %s paga $ %,d y su devuelta es $ %,d. %n",
+                  mesa.getNumero(), efectivo, devuelta);
+      } catch (EfectivoInsuficienteException ex) {
+          System.err.println(ex.getMessage());
+      }
+  }
 }
--- src/main/java/com/dkdev/Model/var.java
+++
@@ -1,5 +0,0 @@
-package com.dkdev.Model;
-
-public enum var {
-
-}
